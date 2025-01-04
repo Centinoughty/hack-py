f712274a13c18e9ca0fed940e30951a02cbc5958
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-01-04 15:42:10 +0530

    added copy button

--- hack-front/src/app/page.tsx
+++ hack-front/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from "react";
 import { BiLinkAlt, BiSend } from "react-icons/bi";
+import { FaRegCopy } from "react-icons/fa";
 
 interface ResponseProps {
   question: string;
@@ -23,14 +24,14 @@ export default function Home() {
   }, [questions]);
 
   async function handleSubmit() {
-    if (!question.trim()) {
+    if (question.trim() === "") {
       return;
     }
 
     let askedQuestion = question;
     let response;
     try {
-      response = await fetch("https://api.openai.com/v1/completions", {
+      response = await fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -38,24 +39,29 @@ export default function Home() {
         },
         body: JSON.stringify({
           model: "gpt-3.5-turbo",
-          prompt: askedQuestion,
+          messages: [
+            { role: "system", content: "You are a helpful assistant" },
+            { role: "user", content: askedQuestion },
+          ],
           max_tokens: 150,
           temperature: 0.7,
         }),
       });
+
+      const data = await response?.json();
+      console.log(data);
+      setQuestions([
+        ...questions,
+        {
+          question: question.trim(),
+          response: "data.choices[0].message.content",
+        },
+      ]);
     } catch (error) {
       console.log(error);
     }
 
-    const data = await response?.json();
     setQuestion("");
-    setQuestions([
-      ...questions,
-      {
-        question: question.trim(),
-        response: data.choices[0].text.trim(),
-      },
-    ]);
   }
 
   function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
@@ -81,26 +87,38 @@ export default function Home() {
   return (
     <>
       <main className="relative min-h-screen font-mono">
-        <div className="absolute top-2 left-2 text-white tracking-widest text-xl">
+        <div className="fixed top-2 left-2 text-white tracking-widest text-xl">
           AGGIN APP
         </div>
-        <ul className="pt-4 pb-24 mx-auto flex flex-col items-center w-[600px] gap-8">
+        <ul
+          className={`${
+            questions.length && "pt-4 pb-28"
+          } mx-auto flex flex-col items-center w-[600px] gap-8`}
+        >
           {questions.length ? (
             questions.map((question, idx) => (
               <div
                 key={idx}
-                className="text-white flex flex-col items-start gap-2 w-full"
+                className="text-white flex flex-col items-start gap-2 w-full group"
               >
-                <p className="px-4 py-3 ml-auto text-left break-words rounded-3xl bg-white/15 max-w-[300px]">
+                <p className="ml-auto px-4 py-3 text-left break-words rounded-3xl bg-white/15 max-w-[300px]">
                   {question.question}
                 </p>
                 <p className="text-left rounded-3xl rounded-tr-none p-4">
                   {question.response}
                 </p>
+                <button>
+                  <FaRegCopy
+                    size={18}
+                    className="absolute hidden duration-300 transition-all group-hover:block text-white/60 -translate-y-3"
+                  />
+                </button>
               </div>
             ))
           ) : (
-            <div className="">Ask anything</div>
+            <div className="h-screen flex justify-center items-center text-white text-3xl">
+              Ask anything!!
+            </div>
           )}
         </ul>
         <div className="p-2 fixed bottom-10 left-1/2 transform -translate-x-1/2 w-[400px] md:w-[500px] lg:w-[600px] rounded-full flex items-center gap-2 border-2 border-gray-500 bg-black/50 backdrop-blur-md">
